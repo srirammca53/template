1c1b64b42bb9c32adb768e289fe3b4b71d5f3841
sriram <sriram.in20@gmail.com>
2011-07-29 11:19:54 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onetoone::Application.config.secret_token = '8203c9ad4279653492dd80c8cb1851e751ff48396b3fb014b992ed7337ba379464eaeb8bc8ed3c45cd163da6e2009e0e02c91711d6d5b664b8869f30a2d63a2d'
